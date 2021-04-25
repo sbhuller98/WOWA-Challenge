c585146323f077451fd33e597bf92d001dff89b3
Sukh <brownie2011@live.ca>
2021-04-25 16:37:40 -0700

    added logos, not sure why some images are not showing up on certain filters

--- frontend/src/IndividualRateDetail.tsx
+++ frontend/src/IndividualRateDetail.tsx
@@ -1,6 +1,7 @@
 
 import axios from 'axios'
 import { stringify } from 'node:querystring';
+import Image from 'next/image'
 
 interface Props {
     lender: string;
@@ -14,37 +15,37 @@ interface Props {
 
   //maps company name to image file
 const nameToImage = {
-    'Alterna Savings': 'alterna-savings.png',
-    'ATB': 'atb-financial.png',
-    'Banyan Mortgage': 'banyan-mortgage.png',
-    'BMO': 'bmo.png',
-    'Canada Life': 'canada-life-logo.svg',
-    'Canadian Western': 'canadian-western-bank.png',
-    'CIBC': 'cibc.svg',
-    'Citadel Mortgages': 'citadel-mortgages.png',
+    'Alterna Savings':'alterna-savings.png',
+    'ATB':'atb-financial.png',
+    'Banyan Mortgage':'banyan-mortgage.png',
+    'BMO':'bmo.png',
+    'Canada Life':'canada-life-logo.svg',
+    'Canadian Western':'canadian-western-bank.png',
+    'CIBC':'cibc.svg',
+    'Citadel Mortgages':'citadel-mortgages.png',
     'CMLS': 'cmls.png',
     'Coast Capital': 'coast-capital.png',
     'Desjardins':'desjardins.png',
     'DUCA':'duca.png',
     'Equitable':'equitable.svg',
-    'First National': 'first-national.png',
-    'First Ontario': 'firstOntario.svg',
-    'HSBC': 'hsbc.png',
-    'ICICI': 'ICICI.png',
-    'Investors Group': 'investorsgroup.png',
-    'Laurentian': 'laurentian.svg',
-    'Manulife': 'manulife.svg',
-    'MCAP':'macp=small.png',
+    'First National':'first-national.png',
+    'First Ontario':'firstOntario.svg',
+    'HSBC':'hsbc.png',
+    'ICICI':'ICICI.png',
+    'Investors Group':'investorsgroup.png',
+    'Laurentian':'laurentian.svg',
+    'Manulife':'manulife.svg',
+    'MCAP':'mcap-small.png',
     'Meridian':'meridian.svg',
     'Metro Mortgage':'metro-mortgage-logo.png',
-    'motusbank':'motusbank.org',
+    'motusbank':'motusbank.png',
     'National':'national.svg',
     'nuborrow':'nuborrow.png',
-    'Peoples Bank': 'peoples-bank.png',
+    'Peoples Bank':'peoples-bank.png',
     'RBC': 'rbc.png',
-    'Scotiabank': 'scotiabank.png',
-    'Simplii Financial': 'simplii.png',
-    'Tangerine': 'tangerine.png',
+    'Scotiabank':'scotiabank.png',
+    'Simplii Financial':'simplii.png',
+    'Tangerine':'tangerine.png',
     'TD':'td.png',
     'Westworth Financial': 'westworth.png'
 }
@@ -52,9 +53,11 @@ const nameToImage = {
 
 
 const IndividualRateDetail = ( {lender, rate, payment, term, type, correctType, correctTerm} ): JSX.Element => {
-    
+    console.log(nameToImage[lender])
+     const imageSRC = "/logos/" + nameToImage[lender]
     return(
         <div className="inline-flex space-x-14 border border-blue-300 p-3">
+            <Image src={imageSRC} width={500} height={500}/>
             <h3 className ="flex text-base md:text-lg font-light" >{lender}</h3>
             <h3 className ="flex text-base md:text-lg font-light" >{rate}</h3>
             <h3 className ="flex text-base md:text-lg font-light" >{payment}</h3>
